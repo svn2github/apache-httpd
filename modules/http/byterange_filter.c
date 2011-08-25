svn2github/apache-httpd

commit 8a6dc6fe097e6adef6ea7f87fffe7044f6fb378d

avoid inserting the same bucket into bbout twice, causing an endless loop
git-svn-id: http://svn.apache.org/repos/asf/httpd/httpd/trunk@1161767 13f79535-47bb-0310-9956-ffa450edef68

modules/http/byterange_filter.c
@@ -195,8 +195,8 @@ static apr_status_t copy_brigade_range(apr_bucket_brigade *bb,
             return rv;
         }
 
-        APR_BRIGADE_INSERT_TAIL(bbout, copy);
         if (e == first) {
+            APR_BRIGADE_INSERT_TAIL(bbout, copy);
             if (off_first != start64) {
                 rv = apr_bucket_split(copy, (apr_size_t)(start64 - off_first));
                 if (rv == APR_ENOTIMPL) {

commit 9b00a0389fdf45c4c7e24dc5b37e9a0a8c8d1161

fix new breakage introduced by r1161767
git-svn-id: http://svn.apache.org/repos/asf/httpd/httpd/trunk@1161778 13f79535-47bb-0310-9956-ffa450edef68

modules/http/byterange_filter.c
@@ -195,8 +195,8 @@ static apr_status_t copy_brigade_range(apr_bucket_brigade *bb,
             return rv;
         }
 
+        APR_BRIGADE_INSERT_TAIL(bbout, copy);
         if (e == first) {
-            APR_BRIGADE_INSERT_TAIL(bbout, copy);
             if (off_first != start64) {
                 rv = apr_bucket_split(copy, (apr_size_t)(start64 - off_first));
                 if (rv == APR_ENOTIMPL) {
@@ -251,9 +251,6 @@ static apr_status_t copy_brigade_range(apr_bucket_brigade *bb,
             if (e == first) {
                 off_last += start64 - off_first;
                 copy = out_first;
-            }
-            else {
-                APR_BRIGADE_INSERT_TAIL(bbout, copy);
             }
             if (end64 - off_last != (apr_uint64_t)e->length) {
                 rv = apr_bucket_split(copy, (apr_size_t)(end64 + 1 - off_last));